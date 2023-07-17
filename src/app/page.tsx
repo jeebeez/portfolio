e3bd9d19b12d20d3e086105f6bfd852a2b95b373
Gaurav Bhatt <bhattgauravd@gmail.com>
2023-07-17 11:28:24 +0530

    -added animations

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,87 @@
-import Image from 'next/image'
+"use client";
 
-export default function Home() {
+import Image from "next/image";
+import dynamic from "next/dynamic";
+import AnimatedCursor from "react-animated-cursor";
+import { AnimatePresence, motion } from "framer-motion";
+
+// const AnimatedCursor = dynamic(() => import("react-animated-cursor"), {
+//   ssr: false,
+// });
+
+const Home = () => {
+  const ExpandableDiv = () => {
+    const variants = {
+      initial: { scaleX: 1, transition: { duration: 0.5 } },
+      hover: { scaleX: 1.2, transition: { duration: 0.5 } },
+      rest: { scaleX: 0.8, transition: { duration: 0.5 } },
+    };
+
+    return (
+      <motion.div
+        className="grow shrink basis-0"
+        style={{
+          border: "1px solid #000",
+          flex: 1,
+          margin: 10,
+          height: "100px",
+          width: "500px",
+        }}
+        variants={variants}
+        initial="initial"
+        whileHover="hover"
+        whileTap="rest"
+      />
+    );
+  };
+
+  const ContactCard = (
+    <>
+      <div
+        className={
+          "flex items-center justify-center font-extrabold flex-1 h-full bg-[#f74214] border-[#d9cebe] border text-[#d9cebe]  hover:text-[#f74214] hover:bg-[#d7d7d7] transition-all hover:flex-[15%] duration-500 ease-in-out"
+        }
+      >
+        <div className="absolute">LINKEDIN</div>
+      </div>
+    </>
+  );
+
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
+    <main className="flex min-h-screen  bg-[#f74214] flex-col items-center justify-between p-24">
+      {/* <AnimatedCursor
+        color="255,255,255"
+        innerSize={8}
+        outerSize={70}
+        innerScale={1}
+        outerScale={2}
+        outerAlpha={1}
+        outerStyle={{
+          mixBlendMode: "exclusion",
+        }}
+        innerStyle={{
+          backgroundColor: "#fff",
+          mixBlendMode: "exclusion",
+        }}
+      /> */}
+      <div className="absolute top-10 left-10 flex gap-2 items-center ">
+        <div className="flex duration-300 transition ease-in-out hover:rotate-180">
+          <div className="w-4 h-8 rounded-tl-full border-2 rounded-bl-full bg-slate-100"></div>
+          <div className="w-4 h-8 rounded-tr-full rounded-br-full bg-gray-400"></div>
+        </div>
+        <span>Invert Colors</span>
+      </div>
+      <div className="w-full h-52 group max-w-full flex gap-8">
+        {ContactCard}
+        {ContactCard}
+        {ContactCard}
+      </div>
+      {/* <div style={{ display: "flex", justifyContent: "space-between" }}>
+        <ExpandableDiv />
+        <ExpandableDiv />
+        <ExpandableDiv />
+      </div> */}
+      {/* <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
         <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
           Get started by editing&nbsp;
           <code className="font-mono font-bold">src/app/page.tsx</code>
@@ -15,7 +93,7 @@ export default function Home() {
             target="_blank"
             rel="noopener noreferrer"
           >
-            By{' '}
+            By{" "}
             <Image
               src="/vercel.svg"
               alt="Vercel Logo"
@@ -27,10 +105,9 @@ export default function Home() {
           </a>
         </div>
       </div>
-
       <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
         <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
+          className="relative  "
           src="/next.svg"
           alt="Next.js Logo"
           width={180}
@@ -38,7 +115,6 @@ export default function Home() {
           priority
         />
       </div>
-
       <div className="mb-32 grid text-center lg:mb-0 lg:grid-cols-4 lg:text-left">
         <a
           href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
@@ -47,7 +123,7 @@ export default function Home() {
           rel="noopener noreferrer"
         >
           <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
+            Docs{" "}
             <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
               -&gt;
             </span>
@@ -64,7 +140,7 @@ export default function Home() {
           rel="noopener noreferrer"
         >
           <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{' '}
+            Learn{" "}
             <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
               -&gt;
             </span>
@@ -81,7 +157,7 @@ export default function Home() {
           rel="noopener noreferrer"
         >
           <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{' '}
+            Templates{" "}
             <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
               -&gt;
             </span>
@@ -98,7 +174,7 @@ export default function Home() {
           rel="noopener noreferrer"
         >
           <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
+            Deploy{" "}
             <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
               -&gt;
             </span>
@@ -107,7 +183,9 @@ export default function Home() {
             Instantly deploy your Next.js site to a shareable URL with Vercel.
           </p>
         </a>
-      </div>
+      </div> */}
     </main>
-  )
-}
+  );
+};
+
+export default Home;
