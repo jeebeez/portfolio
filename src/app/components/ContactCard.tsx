6a967c1a35b1dd422fe452146850e6eee7b182d0
Gaurav Bhatt <bhattgauravd@gmail.com>
2023-07-19 19:11:51 +0530

    -added contact details

--- src/app/components/ContactCard.tsx
+++ src/app/components/ContactCard.tsx
@@ -1,19 +1,37 @@
-interface ContactCardProps {
+import Link from "next/link";
+
+export interface ContactCardProps {
   title: string;
-  text: string;
-  subText: string;
+  subTitle: string;
+  url: string;
 }
 
-const ContactCard = ({ title, text, subText }: ContactCardProps) => {
+const ContactCard = ({ title, subTitle, url }: ContactCardProps) => {
   return (
     <>
-      <div
+      <Link
         className={
-          "flex items-center h-64 justify-center font-extrabold flex-1 bg-black border-[#d9cebe] border text-[#d9cebe] hover:text-[#f74214] hover:bg-[#d7d7d7] transition-all hover:flex-[22%] duration-700 ease-in-out"
+          "flex relative h-64 font-extrabold flex-1 bg-black border-[#d9cebe] border text-[#d9cebe] hover:bg-[#d7d7d7] transition-all hover:flex-[22%] duration-500 ease-in-out group p-6"
         }
+        href={url}
+        target={"_blank"}
       >
-        <div className="text-4xl">title</div>
-      </div>
+        <div className="transition-all flex ease-in-out group-hover:translate-y-0 -translate-y-[50%] opacity-0 group-hover:opacity-100 duration-500 text-5xl absolute text-black bottom-5 right-5">
+          ↗︎
+        </div>
+
+        <div className="transition-all flex ease-in-out text-black opacity-0 group-hover:opacity-100 duration-500 flex-col items-start">
+          <div className="text-2xl transition-all ease-in-out group-hover:translate-y-0 duration-500 -translate-y-[50%]">
+            {title}
+          </div>
+          <div className="font-extralight text-start ">{subTitle}</div>
+        </div>
+        <div className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2">
+          <div className="translate-y-0 group-hover:-translate-y-[100%] group-hover:opacity-0 duration-500 text-4xl">
+            {title}
+          </div>
+        </div>
+      </Link>
     </>
   );
 };
---
+++ src/app/data/consts.ts
@@ -0,0 +1,19 @@
+import { ContactCardProps } from "../components/ContactCard";
+
+export const contactCardInfo: ContactCardProps[] = [
+  {
+    title: "Github",
+    subTitle: "Explore my code and contributions",
+    url: "https://github.com/jeebeez",
+  },
+  {
+    title: "Linkedin",
+    subTitle: "My professional life",
+    url: "https://www.linkedin.com/in/bhattgauravd/",
+  },
+  {
+    title: "Email",
+    subTitle: "Quick way to reach me",
+    url: "mailto:bhattgauravd@gmail.com",
+  },
+];
