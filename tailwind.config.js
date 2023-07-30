b5ce689705a6d0a6dd9f67c51cb7c981d0b6a002
Gaurav Bhatt <bhattgauravd@gmail.com>
2023-07-30 20:55:27 +0530

    chore: added marquee styles

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,8 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
+  variants: {
+    animation: ["motion-safe", "motion-reduce"],
+  },
   content: [
     "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
@@ -14,9 +17,31 @@ module.exports = {
       },
       animation: {
         "spin-slow": "spin 8s linear infinite",
+        marquee: "marquee 50s linear infinite",
+        marquee2: "marquee2 50s linear infinite",
+        "reverse-marquee": "reverse-marquee 50s linear infinite",
+        "reverse-marquee2": "reverse-marquee2 50s linear infinite",
       },
       fontFamily: {
         monument: ["MonumentExtended", "sans-serif"],
+      },
+      keyframes: {
+        marquee: {
+          "0%": { transform: "translateX(0%)" },
+          "100%": { transform: "translateX(-100%)" },
+        },
+        marquee2: {
+          "0%": { transform: "translateX(100%)" },
+          "100%": { transform: "translateX(0%)" },
+        },
+        "reverse-marquee": {
+          "0%": { transform: "translateX(-100%)" },
+          "100%": { transform: "translateX(0%)" },
+        },
+        "reverse-marquee2": {
+          "0%": { transform: "translateX(0%)" },
+          "100%": { transform: "translateX(100%)" },
+        },
       },
     },
   },
